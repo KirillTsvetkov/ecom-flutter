6e34bab5247229586a07f75297ba78438be72351
Kiril Tsvetkov <kirill.tsvetkov31@gmail.com>
2023-09-14 17:02:42 +0300

    add scroll with pagination for food

--- lib/api/food_service.dart
+++ lib/api/food_service.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'package:ecom/generated_code/api_docs.swagger.dart';
 
-Future<ResourcesFoodShow?> getAllFoods(int page, int perPage) {
+Future<List<ResourcesFoodShow$Data$Item>?> getAllFoods(int page, int perPage) {
   var uri = Uri(host: 'localhost', port: 80, scheme: 'http');
   var Api = ApiDocs.create(baseUrl: uri);
   return  Api.apiFoodsGet(page:page, perPage:perPage).then((response) {
     if (response.isSuccessful) {
-      return response.body;
+      return response.body?.data;
     } else {
       throw Exception('Failed to retrieve food items');
     }
--- lib/ui/orders/food_list.dart
+++ lib/ui/orders/food_list.dart
@@ -1,36 +1,59 @@
-
 import 'package:ecom/generated_code/api_docs.swagger.dart';
 import 'package:flutter/material.dart';
 import 'package:ecom/api/food_service.dart';
 import 'package:ecom/ui/orders/food_list_item.dart';
 
-class FoodListWidget extends StatelessWidget {
+class FoodListWidget extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<ResourcesFoodShow?>(
-      future: getAllFoods(1, 10),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            final foods = snapshot.data;
-            final foodsData = foods!.data;
-            return ListView.builder(
-              itemCount: foodsData!.length,
-              itemBuilder: (context, index) {
-                var food = foodsData[index];
-                return FoodWidget(
-                  name: food.name,
-                  description: food.description,
-                );
-              },
-            );
-          }
-        } else {
-          return CircularProgressIndicator();
-        }
+  State<FoodListWidget> createState() => _FoodListWidgetState();
+}
+
+class _FoodListWidgetState extends State<FoodListWidget> {
+ List<ResourcesFoodShow$Data$Item>? foods;
+ int page = 1;
+ final scrollController = ScrollController();
+ @override
+ void initState() {
+   super.initState();
+   scrollController.addListener(_scrollListener);
+   fetchFood(page);
+ }
+ 
+ Future<ResourcesFoodShow$Data$Item?> fetchFood(int page) async {
+   try {
+     var result = await getAllFoods(page, 10) ?? [];
+     print(result);
+     setState(() {
+        foods = [...?foods, ...result];
+     });
+   } catch (e) {
+     // Handle the exception
+   }
+ }
+     
+ @override
+ Widget build(BuildContext context) {
+  if (foods == null) {
+    return CircularProgressIndicator();
+  } else {
+    return ListView.builder(
+      controller: scrollController,
+      itemCount: foods!.length ?? 0,
+      itemBuilder: (context, index) {
+        var food = foods![index];
+        return FoodWidget(
+          name: food.name,
+          description: food.description,
+        );
       },
     );
+  }
+ }
+
+  void _scrollListener() {
+    if(scrollController.position.pixels == scrollController.position.maxScrollExtent){
+      page = page + 1;
+      fetchFood(page);
+    }
   }
 }
